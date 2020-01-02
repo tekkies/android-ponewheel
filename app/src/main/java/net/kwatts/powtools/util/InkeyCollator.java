3c97d90dc65de053642923736c2edfd61de0caa5
Andy Joiner <andy.joiner@tekkies.co.uk>
2020-01-02 13:07:09 -0800

    WIP

--- app/src/main/java/net/kwatts/powtools/util/InkeyCollator.java
+++ app/src/main/java/net/kwatts/powtools/util/InkeyCollator.java
@@ -1,11 +1,62 @@
 package net.kwatts.powtools.util;
 
-class InkeyCollator {
-    public void append(byte[] stream) {
+import java.util.ArrayList;
 
+class InkeyCollator {
+    public static final int EXPECTED_SIZE = 20;
+    private final ArrayList<Byte> inkey;
+    private final byte[] signature;
+
+    public InkeyCollator() {
+        inkey = new ArrayList<Byte>();
+        signature = Util.StringToByteArrayFastest("098e56");
+    }
+
+    public void append(byte[] bytes) {
+        for (Byte item: bytes) {
+            append(item);
+        }
+    }
+
+    private void append(Byte item) {
+        if(inkey.size() == 0) {
+            if(item == signature[0]) {
+                inkey.add(item);
+            }
+        } else if(inkey.size() == 1) {
+            if(item == signature[1]) {
+                inkey.add(item);
+            } else {
+                inkey.clear();
+            }
+        } else if(inkey.size() == 2) {
+            if(item == signature[2]) {
+                inkey.add(item);
+            } else {
+                inkey.clear();
+            }
+        } else if(inkey.size() < EXPECTED_SIZE) {
+            inkey.add(item);
+        }
     }
 
     public boolean isFound() {
-        return false;
+        return inkey.size() == EXPECTED_SIZE;
+    }
+
+    public byte[] get() {
+        byte[] result=null;
+        if(isFound()) {
+            result = toArray(inkey);
+        }
+        return result;
+    }
+
+    private byte[] toArray(ArrayList<Byte> inkey) {
+        byte[] bytes = new byte[inkey.size()];
+        for(int index=0;index<EXPECTED_SIZE;index++) {
+            bytes[index] = inkey.get(index);
+        }
+        return bytes;
     }
 }
--- app/src/test/java/net/kwatts/powtools/util/InkeyCollatorTest.java
+++ app/src/test/java/net/kwatts/powtools/util/InkeyCollatorTest.java
@@ -13,19 +13,32 @@ import uk.co.tekkies.hsm.plantuml.PlantUmlUrlEncoder;
 
 public class InkeyCollatorTest {
 
+    public static final String GOOD_INKEY = "098e56:6ded2854e0bb3ee3eb77e58974ee956227";
+
     @Test
     public void canCollateGoodStream() {
-        byte[] stream = Util.StringToByteArrayFastest("098e56:6ded2854e0bb3ee3eb77e58974ee956227");
+        byte[] stream = Util.StringToByteArrayFastest(GOOD_INKEY);
         InkeyCollator inkeyCollator = new InkeyCollator();
         inkeyCollator.append(stream);
         Assert.assertTrue(inkeyCollator.isFound());
+        Assert.assertArrayEquals(Util.StringToByteArrayFastest(GOOD_INKEY), inkeyCollator.get());
     }
 
     @Test
     public void canCollateBadStream() {
-        byte[] stream = Util.StringToByteArrayFastest("deadbeef:098e56:6ded2854e0bb3ee3eb77e58974ee956227:deadbeef");
+        byte[] stream = Util.StringToByteArrayFastest("deadbeef:"+GOOD_INKEY+":deadbeef");
         InkeyCollator inkeyCollator = new InkeyCollator();
         inkeyCollator.append(stream);
         Assert.assertTrue(inkeyCollator.isFound());
+        Assert.assertArrayEquals(Util.StringToByteArrayFastest(GOOD_INKEY), inkeyCollator.get());
+    }
+
+    @Test
+    public void canCollatepartialKey() {
+        byte[] stream = Util.StringToByteArrayFastest("deadbeef:098e:"+GOOD_INKEY+":deadbeef");
+        InkeyCollator inkeyCollator = new InkeyCollator();
+        inkeyCollator.append(stream);
+        Assert.assertTrue(inkeyCollator.isFound());
+        Assert.assertArrayEquals(Util.StringToByteArrayFastest(GOOD_INKEY), inkeyCollator.get());
     }
 }
